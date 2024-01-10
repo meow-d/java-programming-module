5bbdb4507b58bcdeb1c5fc4b3d0a3f591b0da9f5
meow-d <isaacwenyi@gmail.com>
2024-01-10 11:01:17 +0800

    first commit

---
+++ week 1/test.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        String name = "World";
+        int n1 = 1, n2 = 2, total;
+        total = n1 + n2;
+        System.out.println("Hello " + name + total);
+    }
+}
