8cdfdef87d3f3c157abfacc5927fca9dc328cc05
meow-d <isaacwenyi@gmail.com>
2024-02-06 10:16:04 +0800

    lab 6

---
+++ lab6/ex2.java
@@ -0,0 +1,21 @@
+package lab6;
+
+public class ex2 {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int smallest = arr[0], largest = arr[0] ;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("smallest: " + smallest);
+        System.out.println("largest: " + largest);
+    }
+}
---
+++ lab6/ex3.java
@@ -0,0 +1,16 @@
+package lab6;
+
+public class ex3 {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int[] arr2 = { 1, 2, 3, 4, 5 };
+        int sum = 0;
+
+        assert arr.length == arr2.length : "Arrays must have the same length";
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i] * arr2[i];
+        }
+        System.out.println("Sum of arrays: " + sum);
+    }
+}
